2f55896579c1001ba64b7c20ef84bf4312ecf125
JohnOmoha <omoha.john@yahoo.com>
2023-03-28 11:36:41 +0200

    create betty-doc doucumentations for process_unsigned_long

--- process_unsigned_long.c
+++ process_unsigned_long.c
@@ -1,6 +1,13 @@
 #include <stdarg.h>
 #include "main.h"
 
+/** 
+ * print_unsigned_long - Gets the length of an unsigned long
+ *
+ * @val: The unsigned long to get the length of
+ *
+ * Return: The length of the unsigned long
+ */
 void print_unsigned_long(unsigned long val)
 {
 	if (val > 9)
@@ -8,6 +15,14 @@ void print_unsigned_long(unsigned long val)
 	_putchar(val % 10 + '0');
 }
 
+/** 
+ * get_unsigned _long_length - Gets the length of an
+ *  unsigned long
+ * 
+ * @val: The unsigned long to get the length of
+ *
+ * Return: The length of the unsigned long
+ */
 int get_unsigned_long_length(unsigned long val)
 {
 	int len = 0;
