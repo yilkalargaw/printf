beb322323bf311b756dbcb014235c0412e956ffd
JohnOmoha <omoha.john@yahoo.com>
2023-03-28 11:17:18 +0200

    add protorypes for the three new functions inside main.h

--- main.h
+++ main.h
@@ -2,8 +2,13 @@
 #define MAIN_H
 
 int _putchar(char c);
+
 void print_integer(int val);
+void print_double(double val);
+void print_string(char *val);
+
 int get_integer_length(int val);
+int get_double_length(double val);
 int _strlen(char *s);
 
 #endif /* MAIN_H */
