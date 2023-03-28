2d89e118cf1238b9c5971efd4b1515a398ac47d4
JohnOmoha <omoha.john@yahoo.com>
2023-03-29 01:51:30 +0200

    added git process_rev for custom reversed string

---
+++ process_rev.c
@@ -0,0 +1,20 @@
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * print_rev - print a string reverse
+ * @s: pointer to char input
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != 0)
+		i++;
+	for (i = i - 1; i >= 0; i--)
+		_putchar(*(s + i));
+	_putchar(10);
+}
